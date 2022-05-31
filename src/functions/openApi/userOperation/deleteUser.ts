72dd0043a59769c6b954ddf9f7560ea2155f7479
toahabhuiyan01 <toahabhuiyan@gmail.com>
2022-06-01 01:15:08 +0600

    try to normalize query params, duplicate code remove

--- src/functions/openApi/userOperation/deleteUser.ts
+++ src/functions/openApi/userOperation/deleteUser.ts
@@ -2,6 +2,7 @@ import 'source-map-support/register';
 import * as Lambda from 'aws-lambda';
 import { AppDataSource } from '../../dbSrc/data-source';
 import { IUser, User } from '../../dbSrc/entity/User';
+import readQueryReadUser from '../utils/readQueryReadUser';
 
 
 interface userbyid {
@@ -10,38 +11,15 @@ interface userbyid {
 
 export default async (c, event: Lambda.APIGatewayProxyEvent, context: Lambda.Context) => {
     try {
-        if(!AppDataSource.isInitialized)
-            await AppDataSource.initialize();
+        
+
+        const {usersToFetch, userById, error} = await readQueryReadUser(c);
+
+        if(error) {
+            return error;
+        }
 
         const userRepo = AppDataSource.getRepository(User);
-
-        const receivedUsers: IUser | null = c.request.query;
-
-        let userBulkData: IUser[] = [];
-        let userById: userbyid = {};
-
-        const userIds: string[] = receivedUsers?.id?.split(",") || [];
-        const userPhones: string[] = receivedUsers?.phoneNumber?.split(",") || [];
-
-        if(userIds?.length !== userPhones?.length) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({
-                    message: "Please provide same amount of user information."
-                })
-            }
-        }
-
-        for (let i = 0; i < userIds.length; i++) {
-            if(userIds[i] && userPhones[i]) {
-                userBulkData.push({id: userIds[i]});
-                userById[userIds[i]] = userPhones[i];
-            }
-        }
-
-        const usersToFetch = await userRepo.find({
-            where: userBulkData
-        });
 
         if(usersToFetch) {
             let userToDelete: User[] = [];
---
+++ src/functions/openApi/utils/readQueryReadUser.ts
@@ -0,0 +1,47 @@
+import { AppDataSource } from "../../dbSrc/data-source";
+import { IUser, User } from "../../dbSrc/entity/User";
+
+interface userbyid {
+    [key: string]: string;
+}
+
+export default async (c) => {
+    if(!AppDataSource.isInitialized)
+            await AppDataSource.initialize();
+
+        const userRepo = AppDataSource.getRepository(User);
+
+        const receivedUsers: IUser | null = c.request.query;
+
+        let userBulkData: IUser[] = [];
+        let userById: userbyid = {};
+
+        const userIds: string[] = receivedUsers?.id?.split(",") || [];
+        const userPhones: string[] = receivedUsers?.phoneNumber?.split(",") || [];
+
+        if(userIds?.length !== userPhones?.length) {
+            const error = {
+                statusCode: 400,
+                body: JSON.stringify({
+                    message: "Please provide same amount of user information."
+                })
+            }
+            return {error};
+        }
+
+        for (let i = 0; i < userIds.length; i++) {
+            if(userIds[i] && userPhones[i]) {
+                userBulkData.push({id: userIds[i]});
+                userById[userIds[i]] = userPhones[i];
+            }
+        }
+
+        const usersToFetch: User[] = await userRepo.find({
+            where: userBulkData
+        });
+
+        return {
+            usersToFetch,
+            userById
+        };
+}
