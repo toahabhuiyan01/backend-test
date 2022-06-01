b20e690a5bc12c664146225a61fc8b3c1fae3e29
toaha.bhuiyan <toaha.bhuiyan@shadhinlab.com>
2022-06-01 17:12:08 +0600

    updated the test cases and write some on unhappy path

--- app.test.ts
+++ app.test.ts
@@ -1,12 +1,16 @@
 import { spawn, ChildProcess } from 'child_process';
 import axios, { AxiosInstance } from 'axios';
 import waitOn from 'wait-on';
+import { nanoid } from 'nanoid';
 
 jest.setTimeout(15000);
 
 describe('serverless framework example', () => {
   let start: ChildProcess;
   let client: AxiosInstance;
+
+  let ids: any = [];
+  let phoneNumbers: any = [];
 
   beforeAll(async () => {
     client = axios.create({ baseURL: 'http://localhost:3000', validateStatus: () => true });
@@ -17,28 +21,43 @@ describe('serverless framework example', () => {
   // afterAll(() => process.kill(start.pid));
 
   test('GET /dev/users returns 200 with array of all users', async () => {
-    const res = await client.get('/dev/users');
-    expect(res.status).toBe(200);
+    const res1 = await client.get('/dev/users');
+    const promises = [0, 0, 3, 3, 3].map(async () => {
+      const phoneNumber = nanoid(11);
+      const user = await client.post('/dev/users', {phoneNumber});
+      ids.push(user.data.user.id);
+      phoneNumbers.push(user.data.user.phone_no);
+    })
+    await Promise.all(promises);
+
+    const res2 = await client.get('/dev/users');
+
+
+    expect(res2.data.length - res1.data.length - 5).toBe(0);
+    expect(ids.length).toBe(5);
+
   });
 
-  test('POST /dev/users returns 201 creating after creating a user', async () => {
-    const res = await client.post('/dev/users', {phoneNumber: "0122222654615222"});
-    expect(res.status).toBe(201);
+  test('PATCH /dev/users returns 200 as no query params was given', async () => {
+    const res = await client.patch(`/dev/users?id=${ids.join(",")}`);
+    expect(res.status).toBe(400);
+  });
+
+  test('PATCH /dev/users returns 200 as no query params was given', async () => {
+    const res = await client.patch(`/dev/users?id=${ids.join(",")}&phoneNumber=${phoneNumbers.join(",")}`);
+    expect(res.status).toBe(200);
+    expect(res.data.updated.length).toBe(5);
+  });
+
+  test('DELETE dev/users returns 200 after delete that user', async () => {
+    const res = await client.delete(`/dev/users?id=${ids.join(",")}`);
+    expect(res.status).toBe(200);
+    expect(res.data.deleted.length).toBe(5);
   });
 
   test('PATCH /dev/users returns 400 as no query params was given', async () => {
     const res = await client.patch('/dev/users');
     expect(res.status).toBe(400);
-  });
-
-  test('DELETE dev/users returns 200 after delete that user', async () => {
-    const res = await client.delete('/dev/users?phoneNumber=0122222654615222');
-    expect(res.status).toBe(200);
-  });
-
-  test('DELETE dev/users returns 400 after try to  delete a deleted user', async () => {
-    const res = await client.delete('/dev/users?phoneNumber=0122222654615222');
-    expect(res.status).toBe(200);
   });
 
 });
